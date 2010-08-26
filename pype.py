80ef3ef9d9c3b477203ce53ab72f1d4b1885145e
Julien <github@mandark.fr>
2010-08-26 23:49:02 +0200

    Refactoring documentation using doctest

--- pype.py
+++ pype.py
@@ -1,112 +1,202 @@
 #!/usr/bin/env python
-"""
-Infix programming syntax
+""" Infix programming toolkit
 
-Utilities enablig an infix syntax like this :
-
-euler2 = fib() | where(lambda x: x % 2 == 0)
-           | take_while(lambda x: x < 4000000)
-           | sum
-
-assert euler2 == 4613732
-
-Resolving the 2nd Euler Project exercise :
+Module enablig an infix syntax, as an exemple,
+resolving the 2nd Euler Project exercise:
 "Find the sum of all the even-valued terms in Fibonacci
  which do not exceed four million."
 
-Each Pipe is useable as, for exemple sum :
-[1, 2, 3] | sum
+euler2 = fib() | where(lambda x: x % 2 == 0)
+               | take_while(lambda x: x < 4000000)
+               | add
 
-Each FuncPipe is useable as, for exemple where :
-[1, 2, 3] | where(lambda x: x % 2 == 0)
-A FuncPipe is nothing more than a Parametrized Pipe,
-or a function returning a specific Pipe
+The basic symtax is to use a Pipe like in a shell :
+>>> [1, 2, 3] | add
+6
+
+Each FuncPipe is a function acting like a Pipe, for exemple where :
+>>> [1, 2, 3] | where(lambda x: x % 2 == 0) #doctest: +ELLIPSIS
+<generator object <genexpr> at ...>
+
+A FuncPipe is nothing more than a function returning a specialized Pipe,
 
 You can construct your pipes using Pipe and FuncPipe classes like :
 
 stdout = Pipe(lambda x: sys.stdout.write(str(x)))
 select = FuncPipe(lambda iterable, pred: (pred(x) for x in iterable))
 
-Available Pipes are :
+Or using decorators :
+@Pipe
+def stdout(x):
+    sys.stdout.write(str(x))
 
-stdout : Outputs anything to the standard output
-  Usage : "42" | stdout
 
-lineout : Outputs anything to the standard output followed by a line break
-  Usage : 42 | lineout
+stdout
+    Outputs anything to the standard output
+    >>> "42" | stdout
+    42
 
-average : Returns the average of the preceding Iterable
-  Usage : print [1, 2, 3, 4, 5, 6] | average
+lineout
+    Outputs anything to the standard output followed by a line break
+    >>> 42 | lineout
+    42
 
-count : Returns the length of the preceding Iterable, counting elements one by one
+concat()
+    Aggregates strings using given separator, or ", " by default
+    >>> [1, 2, 3, 4] | concat()
+    '1, 2, 3, 4'
 
-sum : Returns the sum of all elements in the preceding Iterable
+average
+    Returns the average of the given iterable
+    >>> [1, 2, 3, 4, 5, 6] | average
+    3.5
 
-first : Returns the first element of the preceding Iterable
+count
+    Returns the length of the given iterable, counting elements one by one
+    >>> [1, 2, 3, 4, 5, 6] | count
+    6
 
-chain : Unfold preceding Iterable of Iterables
-  Usage : [[1, 2], [3, 4], [5]] | chain
-      Gives [1, 2, 3, 4, 5]
-  Warning : chain only unfold Iterable containing ONLY Iterables :
+add
+    Returns the sum of all elements in the preceding iterable
+    >>> (1, 2, 3, 4, 5, 6) | add
+    21
+
+first
+    Returns the first element of the given iterable
+    >>> (1, 2, 3, 4, 5, 6) | first
+    1
+
+chain
+    Unfold preceding Iterable of Iterables
+    >>> [[1, 2], [3, 4], [5]] | chain | concat()
+    '1, 2, 3, 4, 5'
+
+  Warning : chain only unfold iterable containing ONLY iterables :
       [1, 2, [3]] | chain
           Gives a TypeError: chain argument #1 must support iteration
           Consider using traverse
 
-traverse : Recursively unfold Iterables
-  Usage : [[1, 2], [[[3], [[4]]], [5]]] | traverse
-      Gives 1, 2, 3, 4, 5
+traverse
+    Recursively unfold iterables
+    >>> [[1, 2], [[[3], [[4]]], [5]]] | traverse | concat()
+    '1, 2, 3, 4, 5'
 
-Available FuncPipes are :
-select : Apply a conversion expression given as parameter to each element of the preceding Iterable
-  Usage : [1, 2, 3] | select(lambda x: x * x)
+select()
+    Apply a conversion expression given as parameter
+    to each element of the given iterable
+    >>> [1, 2, 3] | select(lambda x: x * x) | concat()
+    '1, 4, 9'
 
-where : Only yields the matching items of the preceding Iterable:
-  Usage : [1, 2, 3] | where(lambda x: x % 1 == 0)
+where()
+    Only yields the matching items of the given iterable
+    >>> [1, 2, 3] | where(lambda x: x % 2 == 0) | concat()
+    '2'
 
-take_while : Like itertools.takewhile, yields elements of the preceding iterable while the predicat is true :
-  Usage : [1, 2, 3] | take_while(lambda x: x < 3)
+take_while()
+    Like itertools.takewhile, yields elements of the
+    given iterable while the predicat is true
+    >>> [1, 2, 3, 4] | take_while(lambda x: x < 3) | concat()
+    '1, 2'
 
-skip_while : Like itertools.dropwhile, skips elements of the preceding iterable while the predicat is true, then yields others
+skip_while()
+    Like itertools.dropwhile, skips elements of the given iterable
+    while the predicat is true, then yields others
+    >>> [1, 2, 3, 4] | skip_while(lambda x: x < 3) | concat()
+    '3, 4'
 
-chain_with : Like itertools.chain, yields elements of the preceding iterable, then yields elements of its parameters
-  Usage : (1, 2, 3) | chain_width([4, 5, 6]) gives (1, 2, 3, 4, 5, 6)
+chain_with()
+    Like itertools.chain, yields elements of the given iterable,
+    then yields elements of its parameters
+    >>> (1, 2, 3) | chain_with([4, 5], [6]) | concat()
+    '1, 2, 3, 4, 5, 6'
 
-take : Yields the given quantity of elemenets from the preceding iterable :
-  Usage : (1, 2, 3, 4, 5) | take(2) gives (1, 2)
+take()
+    Yields the given quantity of elemenets from the given iterable
+    >>> (1, 2, 3, 4, 5) | take(2) | concat()
+    '1, 2'
 
-skip : Skips the given quantity of elements from the preceding iterable, then yields :
-  Usage : (1, 2, 3, 4, 5) | skip(2) gives (3, 4, 5)
+skip()
+    Skips the given quantity of elements from the given iterable, then yields
+    >>> (1, 2, 3, 4, 5) | skip(2) | concat()
+    '3, 4, 5'
 
-islice : like itertools.islice
-  Usage : assert((1, 2, 3, 4, 5, 6, 7, 8, 9) | islice(2, 8, 2) | sum == (3 + 5 + 7))
+islice()
+    Just the itertools.islice
+    >>> (1, 2, 3, 4, 5, 6, 7, 8, 9) | islice(2, 8, 2) | concat()
+    '3, 5, 7'
 
-izip : Like itertools.izip
-  Usage : assert((1,2,3,4,5,6,7,8,9)
-             | izip([9,8,7,6,5,4,3,2,1])
-             | select(lambda x : x[0] * x[1])
-             | sum
-               == (1*9 + 2*8 + 3*7 + 4*6 + 5*5 + 6*4 + 7*3 + 8*2 + 9*1))
+izip()
+    Just the itertools.izip
+    >>> (1, 2, 3, 4, 5, 6, 7, 8, 9) \
+            | izip([9, 8, 7, 6, 5, 4, 3, 2, 1]) \
+            | concat()
+    '(1, 9), (2, 8), (3, 7), (4, 6), (5, 5), (6, 4), (7, 3), (8, 2), (9, 1)'
 
-aggregate : Works as python reduce
-  Usage :     assert(one2nine | aggregate(lambda x, y: x * y) == 1*2*3*4*5*6*7*8*9)
-        assert(one2nine | aggregate(lambda x, y: str(x) + ', ' + str(y)) == "1, 2, 3, 4, 5, 6, 7, 8, 9")
+aggregate()
+    Works as python reduce
+    >>> (1, 2, 3, 4, 5, 6, 7, 8, 9) | aggregate(lambda x, y: x * y)
+    362880
 
-concat : A utility to aggregate strings using given separator, works like aggregate(lambda x, y: str(x) + separator + str(y))
+    >>> (1, 2, 3, 4, 5, 6, 7, 8, 9) \
+            | aggregate(lambda x, y: str(x) + ', ' + str(y))
+    '1, 2, 3, 4, 5, 6, 7, 8, 9'
 
-any : Returns true if any of the elements matches the given predicat
+any()
+    Returns True if any element of the given iterable satisfies the predicate
+    >>> (1, 3, 5, 6, 7) | any(lambda x: x >= 7)
+    True
 
-all : Returns true if all elements matches the given predicat
+    >>> (1, 3, 5, 6, 7) | any(lambda x: x > 7)
+    False
 
-bigger : Returns the biggest element, using the given comparator
 
-groupby : Like itertools.groupby(sorted(iterable, key = keyfunc), keyfunc)
-Usage : assert(one2nine | groupby(lambda x: x % 2 and "Even" or "Odd")
+all()
+    Returns True if all elements of the given iterable
+    satisfies the given predicate
+    >>> (1, 3, 5, 6, 7) | all(lambda x: x < 7)
+    False
+
+    >>> (1, 3, 5, 6, 7) | all(lambda x: x <= 7)
+    True
+
+max()
+    Returns the biggest element, using the given comparator
+    >>> ('a', 'foo', 'qwerty', 'bar') | max(lambda x, y: len(x) - len(y))
+    'qwerty'
+
+groupby()
+    Like itertools.groupby(sorted(iterable, key = keyfunc), keyfunc)
+    (1, 2, 3, 4, 5, 6, 7, 8, 9) \
+            | groupby(lambda x: x % 2 and "Even" or "Odd")
             | select(lambda x: "%s : %s" % (x[0], (x[1] | concat(', '))))
-            | concat(' / ') == "Even : 1, 3, 5, 7, 9 / Odd : 2, 4, 6, 8")
+            | concat(' / ')
+    'Even : 1, 3, 5, 7, 9 / Odd : 2, 4, 6, 8'
 
-permutations : Returns all possible permutations as :
-  'ABCD' | permutations(2) --> AB AC AD BA BC BD CA CB CD DA DB DC
-  range(3) | permutations() --> 012 021 102 120 201 210
+permutations()
+    Returns all possible permutations
+    >>> 'ABC' | permutations(2) | concat(' ')
+    "('A', 'B') ('A', 'C') ('B', 'A') ('B', 'C') ('C', 'A') ('C', 'B')"
+
+    >>> range(3) | permutations() | concat('-')
+    '(0, 1, 2)-(0, 2, 1)-(1, 0, 2)-(1, 2, 0)-(2, 0, 1)-(2, 1, 0)'
+
+Euler project samples :
+
+    # Find the sum of all the multiples of 3 or 5 below 1000.
+    euler1 = (itertools.count() | select(lambda x: x * 3) | take_while(lambda x: x < 1000) | add) \
+           + (itertools.count() | select(lambda x: x * 5) | take_while(lambda x: x < 1000) | add) \
+           - (itertools.count() | select(lambda x: x * 15) | take_while(lambda x: x < 1000) | add)
+    assert euler1 == 233168
+
+    # Find the sum of all the even-valued terms in Fibonacci which do not exceed four million.
+    euler2 = fib() | where(lambda x: x % 2 == 0) | take_while(lambda x: x < 4000000) | add
+    assert euler2 == 4613732
+
+    # Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
+    square = lambda x: x * x
+    euler6 = square(itertools.count(1) | take(100) | add) - (itertools.count(1) | take(100) | select(square) | add)
+    assert euler6 == 25164150
+
 
 """
 
@@ -115,8 +205,8 @@ import sys
 
 __author__ = 'Julien Palard <julien@eeple.fr>'
 __credits__ = 'Jerome Schneider, for its Python skillz'
-__date__ = '5 February 2010'
-__version__ = '1.1'
+__date__ = '26 Aug 2010'
+__version__ = '1.2'
 
 class Pipe:
     """
@@ -263,12 +353,13 @@ def traverse(args):
             yield arg
 
 @FuncPipe
-def concat(iterable, separator):
+def concat(iterable, separator=", "):
     try:
-        return iterable | aggregate(lambda x, y: str(x) + separator + str(y))
+        return str(iterable | aggregate(lambda x, y: str(x) + separator + str(y)))
     except TypeError:
         #Todo : Checker mieux que ca si la liste est vide avant de faire le aggregate
         return ''
+
 @Pipe
 def stdout(x):
     sys.stdout.write(str(x))
@@ -318,70 +409,5 @@ islice = FuncPipe(itertools.islice)
 izip = FuncPipe(itertools.izip)
 
 if __name__ == "__main__":
-    import operator
-    def fib():
-        x = 1
-        yield 1
-        y = 1
-        yield 1
-        while True:
-            x = x + y
-            yield x
-            y = x + y
-            yield y
-
-    print "Testing Functional Style Python Module v%s" % __version__
-    one2nine = (1,2,3,4,5,6,7,8,9)
-
-    assert(120 == one2nine
-           | where(lambda x: x % 2 == 0)
-           | select(lambda x: x ** 2)
-           | add)
-
-    assert(one2nine | average == ((1+2+3+4+5+6+7+8+9)/9))
-    assert(one2nine | count == len(one2nine))
-    assert(one2nine | add == (1+2+3+4+5+6+7+8+9))
-    assert(one2nine | first == 1)
-    assert(one2nine | select(lambda x: x * 2) | add == (2+4+6+8+10+12+14+16+18))
-    assert(one2nine | where(lambda x: x % 2 == 0) | add == (2+4+6+8))
-    assert(one2nine | take_while(lambda x: x < 5) | add == (1+2+3+4))
-    assert(one2nine | skip_while(lambda x: x < 5) | add == (5+6+7+8+9))
-
-    assert(one2nine | chain_with([10, 11, 12]) | add == (1+2+3+4+5+6+7+8+9+10+11+12))
-    assert(one2nine | take(5) | add == (1+2+3+4+5))
-    assert(one2nine | skip(5) | add == (6+7+8+9))
-    assert(one2nine | islice(2, 8, 2) | add == (3+5+7))
-    assert(one2nine | izip([9,8,7,6,5,4,3,2,1]) | select(lambda x : x[0] * x[1]) | add == (1*9 + 2*8 + 3*7 + 4*6 + 5*5 + 6*4 + 7*3 + 8*2 + 9*1))
-    assert(one2nine | aggregate(lambda x, y: x * y) == 1*2*3*4*5*6*7*8*9)
-    assert(one2nine | aggregate(lambda x, y: str(x) + ', ' + str(y)) == "1, 2, 3, 4, 5, 6, 7, 8, 9")
-    assert(one2nine | any(lambda x: x == 5))
-    assert(one2nine | any(lambda x: x == 10) == False)
-    assert(one2nine | all(lambda x: x < 10))
-    assert(one2nine | all(lambda x: x < 9) == False)
-    assert(one2nine | concat(', ') == "1, 2, 3, 4, 5, 6, 7, 8, 9")
-
-    assert(one2nine | groupby(lambda x: x % 2 and "Even" or "Odd") \
-            | select(lambda x: "%s : %s" % (x[0], (x[1] | concat(', ')))) \
-                | concat(' / ') == "Even : 1, 3, 5, 7, 9 / Odd : 2, 4, 6, 8")
-
-    assert([[1], [2, 3], [4, 5]] | chain | add == (1 + 2 + 3 + 4 + 5))
-    assert([1, [2, 3], [[[4, 5, [6, 7, 8, 9]]]]] | traverse | take(5) | add == (1+2+3+4+5))
-
-    # Find the sum of all the multiples of 3 or 5 below 1000.
-    euler1 = (itertools.count() | select(lambda x: x * 3) | take_while(lambda x: x < 1000) | add) \
-           + (itertools.count() | select(lambda x: x * 5) | take_while(lambda x: x < 1000) | add) \
-           - (itertools.count() | select(lambda x: x * 15) | take_while(lambda x: x < 1000) | add)
-    assert euler1 == 233168
-
-    # Find the sum of all the even-valued terms in Fibonacci which do not exceed four million.
-    euler2 = fib() | where(lambda x: x % 2 == 0) | take_while(lambda x: x < 4000000) | add
-    assert euler2 == 4613732
-
-    # Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
-    square = lambda x: x * x
-    euler6 = square(itertools.count(1) | take(100) | add) - (itertools.count(1) | take(100) | select(square) | add)
-    assert euler6 == 25164150
-
-    assert () | concat('-') == ''
-
-    print "Testing Done !"
+    import doctest
+    doctest.testmod()
