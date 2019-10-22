6579a64203e28b80ccd5e10fa872e6697c28e041
Julien Palard <julien@palard.fr>
2019-10-22 23:44:54 +0200

    Setup bumpversion.

--- setup.py
+++ setup.py
@@ -2,21 +2,21 @@
 
 from setuptools import setup
 
-with open('README.md') as readme_file:
+with open("README.md") as readme_file:
     readme = readme_file.read()
 
 setup(
-    name='pipe',
-    py_modules=['pipe'],
-    version='1.5.0',
-    description='Module enablig a sh like infix syntax (using pipes)',
-    author='Julien Palard',
-    author_email='julien@palard.fr',
-    url='https://github.com/JulienPalard/Pipe',
-    download_url='https://github.com/JulienPalard/Pipe/tarball/master',
+    name="pipe",
+    py_modules=["pipe"],
+    version="1.5.0",
+    description="Module enablig a sh like infix syntax (using pipes)",
+    author="Julien Palard",
+    author_email="julien@palard.fr",
+    url="https://github.com/JulienPalard/Pipe",
+    download_url="https://github.com/JulienPalard/Pipe/tarball/master",
     long_description=readme,
-    long_description_content_type='text/markdown; charset=UTF-8',
-    license='MIT license',
+    long_description_content_type="text/markdown; charset=UTF-8",
+    license="MIT license",
     classifiers=[
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
@@ -25,6 +25,6 @@ setup(
         "License :: OSI Approved :: BSD License",
         "Operating System :: OS Independent",
         "Topic :: Software Development",
-        "Topic :: Software Development :: Libraries :: Python Modules"
-    ]
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
 )
